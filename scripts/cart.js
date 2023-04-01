37ce4993ffd135f8b97a9cee62466efaa9bde3bb
crossovka <cevyacheslav@gmail.com>
2023-04-02 01:43:18 +0900

    ogo

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,30 +1,54 @@
-// $(document).ready(function () {
-// 	var element = $('.cart');
-// 	var height_el = element.offset(10).top;
+// // Получаем элемент, который нужно сделать fixed
+// const element = document.getElementById('cart');
 
-// 	$('.menu__link--cart').css({
-// 		width: element.outerWidth(),
-// 		height: element.outerHeight(),
-// 	});
+// // Получаем координаты элемента, до которого нужно доскроллить
+// const targetOffset = document.querySelector('.intro__logo').offsetTop;
 
-// 	$(window).scroll(function () {
-// 		if ($(window).scrollTop() > height_el) {
-// 			element.addClass('fixed');
-// 		} else {
-// 			element.removeClass('fixed');
-// 		}
-// 	});
+// // Обработчик прокрутки страницы
+// window.addEventListener('scroll', () => {
+// 	// Получаем текущую позицию скролла
+// 	const scrollPosition =
+// 		window.pageYOffset || document.documentElement.scrollTop;
 
+// 	// Проверяем, достигли ли мы элемента, до которого нужно доскроллить
+// 	if (scrollPosition >= targetOffset) {
+// 		// Добавляем класс "fixed" элементу
+// 		element.classList.add('fixed');
+
+// 		// Добавляем класс "visible" элементу с задержкой в 0.5 секунды
+// 		setTimeout(() => {
+// 			element.classList.add('visible');
+// 		}, 500);
+// 	} else {
+// 		// Удаляем классы "fixed" и "visible" у элемента
+// 		element.classList.remove('fixed', 'visible');
+// 	}
 // });
-window.addEventListener('scroll', function () {
-	var cart = document.querySelector('.menu__cart');
-	var menu = document.querySelector('.menu__list');
-	var lastLink = menu.querySelector('.menu__link:last-child');
 
-	var cartOffset = lastLink.offsetTop + lastLink.offsetHeight + 10;
-	if (window.pageYOffset > cartOffset) {
-		cart.style.top = window.pageYOffset - cartOffset + 'px';
-	} else {
-		cart.style.top = '50px';
-	}
-});
+// // Получаем элемент, который нужно скрыть
+// const element = document.getElementById('cart');
+
+// // Получаем координаты элемента, до которого нужно доскроллить
+// const targetOffset = document.querySelector('.intro__logo').offsetTop;
+
+// // Обработчик прокрутки страницы
+// window.addEventListener('scroll', () => {
+// 	// Получаем текущую позицию скролла
+// 	const scrollPosition =
+// 		window.pageYOffset || document.documentElement.scrollTop;
+
+// 	// Проверяем, достигли ли мы элемента, до которого нужно доскроллить
+// 	if (scrollPosition >= targetOffset) {
+// 		// Добавляем класс "fixed" элементу
+// 		element.classList.add('fixed');
+// 	} else {
+// 		// Удаляем класс "fixed" элементу
+// 		element.classList.remove('fixed');
+
+// 		// Задаем время анимации исчезновения элемента
+// 		element.style.transition = 'opacity 0.5s ease-in-out';
+
+// 		// Устанавливаем нулевую прозрачность элемента, чтобы он начал исчезать
+// 		element.style.opacity = 0;
+// 	}
+// });
